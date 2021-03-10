f46d48d938f02053a3a688ce5488420087a34921
showerlee <showerlee@vip.qq.com>
2021-03-10 17:59:59 +0800

    Add iam tf template

--- terraform/ec2/ec2.tf
+++ terraform/ec2/ec2.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "tf_demo_ec2" {
   subnet_id                   = aws_subnet.tf_demo_subnet.id
   key_name                    = var.ec2.key_name
   associate_public_ip_address = true
+  iam_instance_profile        = aws_iam_instance_profile.tf_demo_profile.name
 
   root_block_device {
     volume_type           = var.ec2.volume_type
@@ -16,5 +17,5 @@ resource "aws_instance" "tf_demo_ec2" {
   tags = {
     Name = "${var.app_name}-ec2"
   }
-  depends_on = [aws_vpc.tf_demo_vpc, aws_subnet.tf_demo_subnet, aws_security_group.tf_demo_sg]
+  depends_on = [aws_vpc.tf_demo_vpc, aws_subnet.tf_demo_subnet, aws_security_group.tf_demo_sg, aws_iam_instance_profile.tf_demo_profile]
 }
---
+++ terraform/ec2/iam.tf
@@ -0,0 +1,44 @@
+resource "aws_iam_role" "tf_demo_role" {
+  name               = "${var.app_name}-role"
+  assume_role_policy = data.aws_iam_policy_document.tf_demo_assume_role.json
+}
+
+data "aws_iam_policy_document" "tf_demo_assume_role" {
+  version = "2012-10-17"
+  statement {
+    actions = [
+    "sts:AssumeRole"]
+    principals {
+      type = "Service"
+      identifiers = [
+      "ec2.amazonaws.com"]
+    }
+    effect = "Allow"
+    sid    = ""
+  }
+}
+
+resource "aws_iam_role_policy" "tf_demo_policy" {
+  name   = "tf_demo_policy"
+  role   = aws_iam_role.tf_demo_role.name
+  policy = data.aws_iam_policy_document.tf_demo_policy_document.json
+}
+
+data "aws_iam_policy_document" "tf_demo_policy_document" {
+  version = "2012-10-17"
+  statement {
+    actions = [
+      "ec2:*"
+    ]
+    effect = "Allow"
+    resources = [
+      "*"
+    ]
+  }
+}
+
+resource "aws_iam_instance_profile" "tf_demo_profile" {
+  name = "tf_demo_profile"
+  role = aws_iam_role.tf_demo_role.name
+  depends_on = [aws_iam_role.tf_demo_role]
+}
