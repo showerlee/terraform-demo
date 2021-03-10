aa0f573352636955f03edc2f4d5d4af7abb07f5f
showerlee <showerlee@vip.qq.com>
2021-03-10 18:00:15 +0800

    Add more ec2 info output

--- terraform/ec2/output.tf
+++ terraform/ec2/output.tf
@@ -1,3 +1,15 @@
 output "public_ip" {
   value = aws_instance.tf_demo_ec2[0].public_ip
 }
+
+output "ssh_key" {
+  value = aws_instance.tf_demo_ec2[0].key_name
+}
+
+output "ami" {
+  value = aws_instance.tf_demo_ec2[0].ami
+}
+
+output "instance_type" {
+  value = aws_instance.tf_demo_ec2[0].instance_type
+}
