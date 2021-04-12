f5f55704026fddb9520ca6372c72b28ec5dd34b2
showerlee <showerlee@vip.qq.com>
2021-04-12 17:39:28 +0800

    Add security group as optional

---
+++ terraform/eks-node/securitygroup.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "lt_security_group" {
+  name   = "${local.app_name}-lt-security-group"
+  vpc_id = module.vpc.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Environment = "test"
+    Owner = "Zhenyu.li"
+  }
+}
